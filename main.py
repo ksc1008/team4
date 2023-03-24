df6beac167228f875fe8d6592a85d7f6477bf37a
KSC <ksc1008@naver.com>
2023-03-24 21:27:48 +0900

    transparent window

--- main.py
+++ main.py
@@ -1,16 +1,54 @@
-# This is a sample Python script.
+import sys
+import PyQt6.QtGui
+from PyQt6.QtCore import *
+from PyQt6.QtGui import *
+from PyQt6.QtWidgets import *
+from PyQt6 import QtCore
+from PyQt6 import QtGui
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class CustomWindow(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.mx = 400
+        self.my = 400
+
+    def paintEvent(self, event=None):
+        painter = QPainter(self)
+        self.mx = self.mx + 0.1
+
+        painter.setOpacity(0.2)
+        painter.setBrush(PyQt6.QtGui.QColorConstants.White)
+        painter.setPen(QPen(PyQt6.QtGui.QColorConstants.White))
+        painter.drawRect(self.rect())
+        painter.setOpacity(0.8)
+        painter.drawEllipse(self.mx,self.my,300,300)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+app = QApplication(sys.argv)
 
+# Create the main window
+window = CustomWindow()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+window.setWindowFlags(
+            Qt.WindowType.WindowStaysOnTopHint |
+            Qt.WindowType.FramelessWindowHint |
+            Qt.WindowType.Tool |
+            Qt.WindowType.WindowTransparentForInput)
+window.setAttribute(QtCore.Qt.WidgetAttribute.WA_NoSystemBackground, True)
+window.setAttribute(QtCore.Qt.WidgetAttribute.WA_TranslucentBackground, True)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Create the button
+pushButton = QPushButton(window)
+pushButton.setGeometry(QRect(240, 190, 90, 31))
+pushButton.setText("Finished")
+
+# Center the button
+qr = pushButton.frameGeometry()
+cp = QtGui.QGuiApplication.primaryScreen().availableGeometry().center()
+qr.moveCenter(cp)
+pushButton.move(qr.topLeft())
+
+# Run the application
+window.showFullScreen()
+sys.exit(app.exec())
