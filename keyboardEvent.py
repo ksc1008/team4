79093733ff5252b32108c2c3924134b71400587b
hjinshin <gudwls818@gmail.com>
2023-04-01 01:16:41 +0900

    textLabel 띄우기

--- keyboardEvent.py
+++ keyboardEvent.py
@@ -6,23 +6,29 @@ from PyQt6.QtCore import QThread, pyqtSignal
 
 # 단축키
 class ShorCut(QThread):
-    exit_key = pyqtSignal()
+    quit_key = pyqtSignal()
     circle_key = pyqtSignal()
+    mic_key = pyqtSignal()
+    release_mic_key = pyqtSignal()
+    demo_key = pyqtSignal()
+    label_key = pyqtSignal()
 
     def __init__(self):
         super().__init__()
-        self.maintop = True
         self.running = True
         self.circle_pressing = False
+        self.mic_pressing = False
 
     def run(self):
         while self.running:
             time.sleep(0.1)
-            # exit
-            if win32api.GetAsyncKeyState(0x11) < 0 and win32api.GetAsyncKeyState(0x51) < 0:  # <Ctrl+Q> 입력
-                print("<Ctrl+Q> -> [Exit]")
-                self.exit_key.emit()
 
+            # <Ctrl+Q> 눌리면 -> quit_key
+            if win32api.GetAsyncKeyState(0x11) < 0 and win32api.GetAsyncKeyState(0x51) < 0:  # <Ctrl+Q> 입력
+                print("<Ctrl+Q> -> [quit_key]")
+                self.quit_key.emit()
+
+            # <Ctrl+E> 눌리면 -> circle_key
             if win32api.GetAsyncKeyState(0x11) < 0 and win32api.GetAsyncKeyState(0x45) < 0:  # <Ctrl+E> 입력
                 if not self.circle_pressing:
                     self.circle_key.emit()
@@ -30,6 +36,27 @@ class ShorCut(QThread):
             elif self.circle_pressing:
                 self.circle_pressing = False
 
+            # <Ctrl+M> 눌리면 -> mic_key
+            # <Ctrl+M> 때면 -> mic_key_release
+            if win32api.GetAsyncKeyState(0x11) < 0 and win32api.GetAsyncKeyState(0x4D) < 0:  # <Ctrl+M> 입력
+                if not self.mic_pressing:
+                    print("<Ctrl+M> -> [pressing mic_key]")
+                    self.mic_key.emit()
+                    self.mic_pressing = True
+            elif self.mic_pressing:
+                print("[release mic_key]")
+                self.release_mic_key.emit()
+                self.mic_pressing = False
+
+            # <Ctrl+D> 눌리면 -> demo_key
+            if win32api.GetAsyncKeyState(0x11) < 0 and win32api.GetAsyncKeyState(0x44) < 0:  # <Ctrl+D> 입력
+                print("<Ctrl+D> -> [Demo key]")
+                self.demo_key.emit()
+
+            # <Ctrl+F1> 눌리면 -> label_key
+            if win32api.GetAsyncKeyState(0x11) < 0 and win32api.GetAsyncKeyState(0x70) < 0:  # <Ctrl+F1> 입력
+                print("<Ctrl+F1> -> [label key]")
+                self.label_key.emit()
 
     def stop(self):
         self.running = False
--- mainWindow.py
+++ mainWindow.py
@@ -7,6 +7,7 @@ import overlay_objects.loadingCircle
 from keyboardEvent import ShorCut
 from overlay_animations import animator
 from overlay_objects.overlayObject import OverlayObject
+from textLabel import TextLabel
 
 import demo
 
@@ -30,16 +31,24 @@ class MainWindow(QMainWindow):
 
         self.shortcut = ShorCut()
         self.shortcut.start()
-        self.shortcut.exit_key.connect(self.shortcut_exit_key)
+        self.shortcut.quit_key.connect(self.shortcut_quit_key)
         self.shortcut.circle_key.connect(self.shortcut_circle_key)
+        self.shortcut.demo_key.connect(self.shortcut_demo_key)
+        self.shortcut.label_key.connect(self.shortcut_label_key)
+        self.shortcut.mic_key.connect(self.shortcut_mic_key)
+        self.shortcut.release_mic_key.connect(self.shortcut_release_mic_key)
         self.animator.start()
         self.test = _t
 
         self.lc = None
+        self.re = False
 
+        self.label = TextLabel(self)
+        self.label.setTextLabel(
+            "세잔은 '자연은 표면보다 내부에 있다'고 말하고 정확한 묘사를 하기 위해 사과가 썩을 때까지 그렸다는 일화가 있다. 이처럼 그는 인상파의 사실주의를 추진시켜 단순한 시각적·현상적 사실에서 다시 근본적인 물체의 파악, 즉 자연의 형태가 숨기고 있는 내적 생명을 묘사하는 데 목적을 두었다.")
+        self.label.hide()
 
-
-    #       오버라이드 할 paintEvent
+    # 오버라이드 할 paintEvent
     def paintEvent(self, event=None):
         painter = QPainter(self)
 
@@ -49,17 +58,29 @@ class MainWindow(QMainWindow):
     def closeEvent(self, a0: QtGui.QCloseEvent) -> None:
         self.test.exit(0)
 
-    def addObject(self, obj: OverlayObject):    # 메인 윈도우의 objects에 새 오브젝트를 추가.
+    def addObject(self, obj: OverlayObject):  # 메인 윈도우의 objects에 새 오브젝트를 추가.
         self.objects.append(obj)
         obj.window = self
 
+    def finAllObj(self):
+        for o in self.objects:
+            o.destroy()
+
     @pyqtSlot()
-    def shortcut_exit_key(self):
+    def shortcut_demo_key(self):  # Ctrl + D 입력 시 demo 실행
         # demo.py 참고
         self.addObject(demo.createTestObjectAndApplyAnimation(self.animator))
 
     @pyqtSlot()
-    def shortcut_circle_key(self):             # Ctrl + E 입력 시 Loading Circle 추가 혹은 제거 
+    def shortcut_quit_key(self):  # Ctrl + Q 입력시 프로그램 종료
+        self.finAllObj()
+        self.label.close()
+        self.shortcut.stop()
+        self.shortcut.terminate()
+        self.close()
+
+    @pyqtSlot()
+    def shortcut_circle_key(self):  # Ctrl + E 입력 시 Loading Circle 추가 혹은 제거
         if self.lc is None:
             self.lc = overlay_objects.loadingCircle.LoadingCircle()
             self.addObject(self.lc)
@@ -68,5 +89,19 @@ class MainWindow(QMainWindow):
             self.lc.destroy()
             self.lc = None
 
+    @pyqtSlot()
+    def shortcut_label_key(self):  # Ctrl + F1 입력시 label 보이기 / 숨기기
+        if not self.re:
+            self.label.show()
+            self.re = True
+        else:
+            self.label.hide()
+            self.re = False
 
-# Create the main window
+    @pyqtSlot()
+    def shortcut_mic_key(self):  # Ctrl + M 눌리면
+        print("pressing")
+
+    @pyqtSlot()
+    def shortcut_release_mic_key(self):  # Ctrl + M 때면
+        print("release")
