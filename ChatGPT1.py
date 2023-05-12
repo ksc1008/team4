8438e2620b289cb9c5197ef60bbcd9c136ed9c41
yeogangin <durkddls123@gmail.com>
2023-05-12 11:52:22 +0900

    hyperparameter 추가

--- ChatGPT1.py
+++ ChatGPT1.py
@@ -37,7 +37,7 @@ sttprompt = Queue()
 streaming_queue = Queue()
 
 os.makedirs("history", exist_ok=True)  # history 폴더 생성
-#os.environ['OPENAI_API_KEY'] =  #환경변수에 API_KEY값 지정
+os.environ['OPENAI_API_KEY'] = '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'  #환경변수에 API_KEY값 지정
 openai.api_key = os.getenv("OPENAI_API_KEY")
 #
 messages = [
@@ -47,15 +47,41 @@ messages = [
     }
 ]
 
+#parameter 값을 option.json에 저장
+def save_option():
+    option = {}
+    option['data'] = []
+    option['data'].append({
+        "temperature" : TEMPERATURE,
+        "max_tokens" : MAX_TOKENS
+    })
+    with open("option.json", 'w') as outfile:
+        json.dump(option,outfile)
 
+def change_option():
+    global TEMPERATURE
+    global MAX_TOKENS
+    save_option()
 
+#ChatGPT Hyperparameter option.json 파일 열기 / 생성
+try:
+    with open("option.json", "r") as option_file:
+        option_data = json.load(option_file)
+        TEMPERATURE = option_data['data'][0]['temperature']
+        MAX_TOKENS = option_data['data'][0]['max_tokens']
+except FileNotFoundError:
+    TEMPERATURE = 0.5
+    MAX_TOKENS = 2048
+    save_option()
 
 # ChatGPT API 함수 : ChatGPT 응답을 return
 def query_chatGPT(prompt):
     messages.append({"role": "user", "content": prompt})
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=messages
+        messages=messages,
+        temperature=TEMPERATURE,
+        max_tokens=MAX_TOKENS
     )
     answer = completion["choices"][0]["message"]["content"]
     messages.append({"role": "assistant", "content": answer})
@@ -78,62 +104,6 @@ def save_history(file_name):
             json.dump(text, f)
 
 
-# 음성녹음 함수 : record.wav로 저장
-def voice_recorder():
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("Recording Strated...")
-        audio = r.listen(source)
-        print("Recording Finished")
-    with open("record.wav", "wb") as file:
-        file.write(audio.get_wav_data())
-
-
-# whisper API 함수 : wav파일을 입력받아 text를 return
-def whisper_api(file):
-    transcript = openai.Audio.transcribe("whisper-1", file)
-    text = transcript['text']
-    return text
-
-
-def papago_kte(prompt):  # 파파고를 이용하여 한국어를 영어로 번역하는 함수
-    client_id = "(자신이 가진 파파고 API ID를 입력)"  # 개발자센터에서 발급받은 Client ID 값
-    client_secret = "(자신이 가진 파파고 비번을 입력)"  # 개발자센터에서 발급받은 Client Secret 값
-    encText = urllib.parse.quote(prompt)
-    data = "source=ko&target=en&text=" + encText
-    url = "https://openapi.naver.com/v1/papago/n2mt"
-    request = urllib.request.Request(url)
-    request.add_header("X-Naver-Client-Id", client_id)
-    request.add_header("X-Naver-Client-Secret", client_secret)
-    response = urllib.request.urlopen(request, data=data.encode("utf-8"))
-    rescode = response.getcode()
-    if (rescode == 200):
-        response_body = response.read()
-    else:
-        print("Error Code:" + rescode)
-
-    result = json.loads(response_body.decode('utf-8'))  # json형식으로 온 response를 str형태로 변환
-    return result['message']['result']['translatedText']
-
-
-def papago_etk(prompt):  # 파파고를 이용하여 영어를 한국어로 번역하는 함수
-    client_id = "(자신이 가진 파파고 API ID를 입력)"  # 개발자센터에서 발급받은 Client ID 값
-    client_secret = "(자신이 가진 파파고 비번을 입력)"  # 개발자센터에서 발급받은 Client Secret 값
-    encText = urllib.parse.quote(prompt)
-    data = "source=en&target=ko&text=" + encText
-    url = "https://openapi.naver.com/v1/papago/n2mt"
-    request = urllib.request.Request(url)
-    request.add_header("X-Naver-Client-Id", client_id)
-    request.add_header("X-Naver-Client-Secret", client_secret)
-    response = urllib.request.urlopen(request, data=data.encode("utf-8"))
-    rescode = response.getcode()
-    if (rescode == 200):
-        response_body = response.read()
-    else:
-        print("Error Code:" + rescode)
-
-    result = json.loads(response_body.decode('utf-8'))
-    return result['message']['result']['translatedText']
 
 
 # ==========================================================
@@ -278,7 +248,7 @@ class Streaming(QThread):
         while True:
             if not self.streaming_que.empty():
                 data = self.streaming_que.get()
-                SignalManager().overlaySignals.answer_streaming.emit(data)
+                #SignalManager().overlaySignals.answer_streaming.emit(data)
 
 class Producer(QThread):
     def __init__(self, prompt_que, answer_que):
@@ -345,60 +315,6 @@ class Consumer(QThread):
                 data = self.answer_que.get()
                 SignalManager().overlaySignals.message_arrived.emit(data)
 
-
-class WhisperWorker(QThread):  # Whisper Worker 또한 프로듀서 - 컨슈머 패턴에 추가 -> for concurrency
-    def __init__(self, audio_que, prompt_que):
-        super().__init__()
-        self.audio_que = audio_que
-        self.prompt_que = prompt_que
-        self.overlaySignals = SignalManager().overlaySignals
-
-    def run(self):
-        while True:
-            if not self.audio_que.empty():
-                try:
-                    t = self.audio_que.get()
-                    audio = open("record.wav", "rb")
-                    prompt = whisper_api(audio)
-                    if len(prompt):
-                        self.prompt_que.put(prompt)
-                    else:
-                        self.overlaySignals.throw_error.emit('No prompt found. Please try again.')
-                except openai.error.Timeout as e:
-                    # Handle timeout error, e.g. retry or log
-                    msg = f"OpenAI API request timed out: {e}"
-                    self.overlaySignals.throw_error.emit(msg)
-                    pass
-                except openai.error.APIError as e:
-                    # Handle API error, e.g. retry or log
-                    msg = f"OpenAI API returned an API Error: {e}"
-                    self.overlaySignals.throw_error.emit(msg)
-                    pass
-                except openai.error.APIConnectionError as e:
-                    # Handle connection error, e.g. check network or log
-                    msg = f"OpenAI API request failed to connect: {e}"
-                    self.overlaySignals.throw_error.emit(msg)
-                    pass
-                except openai.error.InvalidRequestError as e:
-                    # Handle invalid request error, e.g. validate parameters or log
-                    msg = f"OpenAI API request was invalid: {e}"
-                    self.overlaySignals.throw_error.emit(msg)
-                    pass
-                except openai.error.AuthenticationError as e:
-                    # Handle authentication error, e.g. check credentials or log
-                    msg = f"OpenAI API request was not authorized: {e}"
-                    self.overlaySignals.throw_error.emit(msg)
-                    pass
-                except openai.error.PermissionError as e:
-                    # Handle permission error, e.g. check scope or log
-                    msg = f"OpenAI API request was not permitted: {e}"
-                    self.overlaySignals.throw_error.emit(msg)
-                    pass
-                except openai.error.RateLimitError as e:
-                    # Handle rate limit error, e.g. wait or log
-                    msg = f"OpenAI API request exceeded rate limit: {e}"
-                    self.overlaySignals.throw_error.emit(msg)
-                    pass
 
 
 class MyWindow(QObject):
